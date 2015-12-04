ad431d70fdea7444dcc6e09dcf63c14ce20d5673
Paradox <Paradox@users.noreply.github.com>
2015-12-04 23:14:15 +0800

    synthetic: sherry ties lijing for both conj/disj

--- agent.py
+++ agent.py
@@ -13,6 +13,10 @@ import logging
 logging.basicConfig(format='%(name)s-%(levelname)s: %(message)s', level=logging.INFO)
 logger = logging.getLogger('Agent')
 
+from environment import contextual_cascading_monkey as contextual_cascading_monkey_environment
+from environment import contextual_full_monkey as contextual_full_monkey_environment
+from environment import contextual_monkey_rng
+
 def movies_mix_cld_adt(L=200):
     children, adults = set(), set()
     with open('movies.csv') as fp:
@@ -85,7 +89,10 @@ def contextual_cascading_monkey(e, s, T=10000):
         recc = random.sample(s.arms, s.K)
         r, _ = e(recc)
         score.append(score[-1] + r)
+    logger.info('Monkey play score {0}/{1}'.format(score[-1], T))
     return score, 0
+
+contextual_full_monkey = contextual_cascading_monkey
 
 def contextual_cascading_sherry(e, s, T=10000):
     delta = 0.9
@@ -97,26 +104,54 @@ def contextual_cascading_sherry(e, s, T=10000):
     X = np.zeros((1, s.d))
     Y = np.zeros(1)
     score = [0]
-    for i in range (1, T):
+    for t in range (1, T):
         x = e()
         U = {arm:theta.dot(x[arm]) + beta * x[arm].dot(np.linalg.inv(V)).dot(x[arm]) for arm in s.arms}
         recc = [p[1] for p in heapq.nlargest(s.K, [(U[arm], arm) for arm in s.arms])]
         r, c = e(recc)
         V += sum([s.gamma ** (2*k) * np.outer(x[recc[k]], x[recc[k]]) for k in range(min(s.K, c+1))])
         X = np.concatenate([X] + [s.gamma ** k * x[recc[k]].reshape(1, s.d) for k in range(min(s.K, c+1))])
-        Y = np.concatenate([Y] + [s.gamma ** k * (k == c) * np.ones(1) for k in range(min(s.K, c+1))])
+        Y = np.concatenate([Y] + [s.gamma ** k * ((k == c) ^ s.disj) * np.ones(1) for k in range(min(s.K, c+1))])
         theta = np.linalg.inv(X.T.dot(X) + lamb * np.eye(s.d)).dot(X.T.dot(Y))
         beta = np.sqrt(np.log(np.linalg.det(V))- ldV - 2 * np.log(delta)) + np.sqrt(lamb)
         score.append(score[-1] + r)
-    return score, 1 - cosine(s.theta, theta) 
+    logger.info('Sherry play score {0}/{1}'.format(score[-1], T))
+    logger.info('theta cosine similarity {0}'.format(1 - cosine(s.theta, theta)))
+    return score, 1 - cosine(s.theta, theta)
+
+def contextual_full_lijing(e, s, T=10000):
+    delta = 0.9
+    lamb = 0.1
+    theta = np.zeros(s.d)
+    beta = 0
+    V = lamb * np.eye(s.d)
+    b = np.zeros(s.d)
+    score = [0]
+    for t in range (1, T):
+        x = e()
+        U = {arm:theta.dot(x[arm]) + beta * x[arm].dot(np.linalg.inv(V)).dot(x[arm]) for arm in s.arms}
+        recc = [p[1] for p in heapq.nlargest(s.K, [(U[arm], arm) for arm in s.arms])]
+        r, ctr = e(recc)
+        V += sum([s.gamma ** (2*k) * np.outer(x[recc[k]], x[recc[k]]) for k in range(len(recc))])
+        b += sum([ctr[k] * x[recc[k]] for k in range(len(recc))])
+        theta = np.linalg.inv(V).dot(b)
+        beta = np.sqrt(s.d * np.log((1 + t*s.L) / delta)) + np.sqrt(lamb)
+        score.append(score[-1] + r)
+    logger.info('Lijing play score {0}/{1}'.format(score[-1], T))
+    logger.info('theta cosine similarity {0}'.format(1 - cosine(s.theta, theta)))
+    return score, 1 - cosine(s.theta, theta)
+
+def flowtest():
+    T = 10000
+    s = contextual_monkey_rng(L=20, d=10, h=0.75, K=4, gamma=0.95, eps=0.1, v=0.35, disj=True)
+    exploit1, explore1 = contextual_cascading_sherry(*contextual_cascading_monkey_environment(s), T=T)
+    exploit2, explore2 = contextual_cascading_monkey(*contextual_cascading_monkey_environment(s), T=T)
+    exploit3, explore3 = contextual_full_monkey(*contextual_full_monkey_environment(s), T=T)
+    exploit4, explore4 = contextual_full_lijing(*contextual_full_monkey_environment(s), T=T)
+    plt.plot(range(T), exploit1, 'r--', range(T), exploit2, 'r--', range(T), exploit4, 'b--')
+    plt.show()
 
 T = 10000
-from environment import contextual_cascading_monkey as contextual_cascading_monkey_environment
-from environment import contextual_monkey_rng
 s = contextual_monkey_rng()
-exploit1, explore1 = contextual_cascading_sherry(*contextual_cascading_monkey_environment(s), T=T)
-print('=========')
-exploit2, explore2 = contextual_cascading_monkey(*contextual_cascading_monkey_environment(s), T=T)
-print(explore1, explore2)
-plt.plot(range(T), exploit1, 'r--', range(T), exploit2, 'b--')
-plt.show()
+flowtest()
+
--- environment.py
+++ environment.py
@@ -7,7 +7,7 @@ import random
 import logging
 logger = logging.getLogger('Environment')
 
-seed = int(time.time())
+seed = int(time.time() * 100) % 339
 np.random.seed(seed)
 logger.info('Numpy Random seed = {0}'.format(seed))
 
@@ -22,6 +22,12 @@ def red(s):
 def uni(x):
     return x/np.sqrt(x.dot(x))
 
+def reward(ctr, gamma, disj):
+    for c, click in enumerate(ctr):
+        if click ^ disj:
+            return int(disj) + gamma ** c * (int(not disj) - int(disj)), c
+    return int(disj), len(ctr)
+
 class ucb_settings:
     def __init__(self, **s):
         self.__dict__ = s
@@ -29,31 +35,41 @@ class ucb_settings:
     def __str__(self):
         return '\n'.join([str(k) + ' ' + str(v) for k, v in self.__dict__.items()])
 
-def contextual_monkey_rng(L=20, d=10, v=0.35):
+def contextual_monkey_rng(L=20, d=10, h=0.35, K=4, gamma=0.95, eps=0.1, v=0.35, disj=False):
     arms = list(range(L))
     x = {arm:np.random.uniform(0, 1, d) for arm in arms}
-    theta = v * uni(np.random.uniform(0, 1, d))
-    return ucb_settings(arms=arms, x=x, theta=theta, d=d)
+    theta = h * uni(np.random.uniform(0, 1, d))
+    return ucb_settings(arms=arms, L=L, x=x, theta=theta, K=K, d=d, gamma=gamma, eps=eps, v=v, disj=disj)
 
-def contextual_cascading_monkey(s=contextual_monkey_rng(), K=4, gamma=0.95, eps=0.1, v=0.35, disj=False):
-    logger.info('Initializing environment "Monkey Contextual"')
+def contextual_cascading_monkey(s=contextual_monkey_rng()):
+    logger.info('Initializing environment "Contextual Cascading Monkey"')
     xt = {arm:s.x[arm] for arm in s.x}
     def environment(recommend=None):
         if recommend == None:
             for arm in xt:
-                xt[arm] = uni(s.x[arm] + np.random.uniform(-v, v, s.d))
+                xt[arm] = uni(s.x[arm] + np.random.uniform(-s.v, s.v, s.d))
             return xt
         else:
-            assert(len(recommend) <= K)
-            #print(sorted([s.theta.dot(xt[arm]) for arm in s.arms])[::-1])
-            #print(red([s.theta.dot(xt[arm]) for arm in recommend]))
-            for c, arm in enumerate(recommend): 
-                p = s.theta.dot(xt[arm]) + np.random.normal(0, eps)
-                if (np.random.uniform(0, 1) < p) ^ disj:
-                    return gamma ** c * int(not disj), c
-            return int(disj), len(recommend)
-    logger.info('Initialized environment "Monkey Contextual"')
-    return environment, ucb_settings(arms=s.arms, K=K, d=s.d, gamma=gamma, theta=s.theta)
+            ctr = [np.random.uniform(0, 1) < s.theta.dot(xt[arm]) + np.random.normal(0, s.eps) for arm in recommend]
+            r, c = reward(ctr, s.gamma, s.disj)
+            return r, c
+    logger.info('Initializing environment "Contextual Cascading Monkey" complete')
+    return environment, ucb_settings(arms=s.arms, L=s.L, K=s.K, d=s.d, gamma=s.gamma, theta=s.theta, disj=s.disj)
+
+def contextual_full_monkey(s=contextual_monkey_rng()):
+    logger.info('Initializing environment "Contextual Full Monkey"')
+    xt = {arm:s.x[arm] for arm in s.x}
+    def environment(recommend=None):
+        if recommend == None:
+            for arm in xt:
+                xt[arm] = uni(s.x[arm] + np.random.uniform(-s.v, s.v, s.d))
+            return xt
+        else:
+            ctr = [(np.random.uniform(0, 1) < s.theta.dot(xt[arm]) + np.random.normal(0, s.eps)) for arm in recommend]
+            r, _ = reward(ctr, s.gamma, s.disj)
+            return r, [int(click) for click in ctr]
+    logger.info('Initializing environment "Contextual Full Monkey" complete')
+    return environment, ucb_settings(arms=s.arms, L=s.L, K=s.K, d=s.d, gamma=s.gamma, theta=s.theta)
 
 def movielens(candidates, userno=1):
     logger.info('Initializing environment "Movielens"')
